994b6f85681b53cf5fed74883ac3d713deb900ff
I A <ia767@nyu.edu>
2021-12-06 00:25:08 -0500

    [2021-aoc6] adding day 6 solution

---
+++ 2021/AOC6.py
@@ -0,0 +1,68 @@
+"""
+Got my best rank yet at 1191 for part 2!
+
+Decided to use a dictionary counting the number of jellyfish
+of a certain age to keep things light and quickly iterate over
+different rounds.
+
+Luckily the position of the jellyfish didn't play a role in either
+part 1 or 2, which allowed me to complete part 2 without having to
+change my code at all (besides adjusting the max number of turns).
+"""
+
+from _utils import read_input
+from collections import defaultdict
+
+
+def parse_input(_input: list):
+    """
+    Open and read file at filepath, return dictionary holding
+    the number of jellyfish of a certain age.
+    """
+    _input = (int(i) for i in _input[0].split(","))  # only one line in input
+    counter = defaultdict(int)
+    for num in _input:
+        counter[num] += 1
+    return counter
+
+
+def simulate_lanternfish_growth(counter: dict, max_days: int):
+    """
+    Return the total number of lanternfish after growing for
+    max_days.
+    """
+    for t in range(max_days):
+        new_counter = defaultdict(int)
+
+        for age, n_jellies in counter.items():
+            if age == 0:
+                new_counter[
+                    8
+                ] = n_jellies  # not updated bc this is only case where new jellyfish are added
+                new_counter[6] += n_jellies
+            else:
+                new_counter[age - 1] += n_jellies
+
+        counter = new_counter
+
+    total_n_jellies = sum(counter.values())
+    return total_n_jellies
+
+
+def main(filepath: str):
+    """
+    Returns solution for AOC day 6 from input filepath.
+    """
+    _input = read_input(filepath)
+    counter = parse_input(_input)
+
+    part_1_score = simulate_lanternfish_growth(counter, 80)
+    part_2_score = simulate_lanternfish_growth(counter, 256)
+
+    return part_1_score, part_2_score
+
+
+if __name__ == "__main__":
+    part_1_score, part_2_score = main("aoc6.txt")
+    print(f"PART 1: {part_1_score}")  # 365862
+    print(f"PART 2: {part_2_score}")  # 1653250886439
---
+++ 2021/test_AOC6.py
@@ -0,0 +1,11 @@
+from AOC6 import main
+
+
+def test_main():
+    test_part_1, test_part_2 = main("test_aoc6.txt")
+    assert test_part_1 == 5934
+    assert test_part_2 == 26984457539
+
+    part_1, part_2 = main("aoc6.txt")
+    assert part_1 == 365862
+    assert part_2 == 1653250886439
