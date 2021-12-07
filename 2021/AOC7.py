2b0eb934e1d2432338427b625910d9cc6eeadc44
I A <ia767@nyu.edu>
2021-12-07 00:28:31 -0500

    [2021-aoc7] using dict for counter, storing min fuel as int

--- 2021/AOC7.py
+++ 2021/AOC7.py
@@ -1,33 +1,39 @@
 """
-Completed in <10 minutes. Needs cleaning + optimization (better data structure
-than list).
+Completed in <10 minutes
 """
 
-from _utils import read_input, timer
+from _utils import read_input, timer, count_occurences
 
 def total_sum(n):
+    """
+    Sum of all numbers from 0 to n.
+
+    Used for the fuel computation in part 2, where n is the distance between
+    two points.
+    """
     return n * (n + 1) // 2
 
-def solver(_input:list, part_1:bool):
-    max_position = max(_input)
-
-    fuel_needed = {}
+def solver(counter:dict, part_1:bool):
+    fuel_needed = 10**16
+    max_position = max(counter.keys())
 
     for target_pos in range(max_position):
         if part_1:
-            total = [abs(cur_pos - target_pos) for cur_pos in _input]
+            total = sum([n * abs(cur_pos - target_pos) for cur_pos, n in counter.items()])
         else:
-            total = [total_sum(abs(1 + cur_pos - target_pos)) for cur_pos in _input]
-        fuel_needed[target_pos] = sum(total)
+            total = sum([n * total_sum(abs(1 + cur_pos - target_pos)) for cur_pos, n in counter.items()])
 
-    return min(fuel_needed.values())
+        fuel_needed = min(fuel_needed, total)
+
+    return fuel_needed
 
 @timer
 def main(filepath: str):
     _input = read_input(filepath)
     _input = [int(i) for i in _input[0].split(",")]
-    part_1_score = solver(_input, part_1=True)
-    part_2_score = solver(_input, part_1=False)
+    counter = count_occurences(_input)
+    part_1_score = solver(counter, part_1=True)
+    part_2_score = solver(counter, part_1=False)
     return part_1_score, part_2_score
 
 
