18a243c9a99036d63678a7ef1fe2746dfa6c24aa
I A <ia767@nyu.edu>
2021-12-08 08:59:50 -0500

    [2021-aoc8] refactoring day 8, extending test coverage

--- 2021/AOC8.py
+++ 2021/AOC8.py
@@ -6,14 +6,64 @@ more involved logic, especially in part 2.
 
 from _utils import read_input, timer
 
-def part_1(_input:list):
-    output_lines = [i.split(' | ')[1] for i in _input]
-    output_lines_len = [[len(i) for i in line.split(' ')] for line in output_lines]
+
+def part_1(_input: list):
+    """
+    Returns the number of signals that are of length 2, 3, 4, or 7
+    (i.e., lengths that allow us to immediately map a signal to a digit).
+    """
+    output_lines = [i.split(" | ")[1] for i in _input]
+    output_lines_len = [
+        [len(i) for i in line.split(" ") if len(i) in {2, 3, 4, 7}]
+        for line in output_lines
+    ]
     output_lines_len = [i for sublist in output_lines_len for i in sublist]
-    part_1_score = len([i for i in output_lines_len if i in {2, 3, 4, 7}])
+    part_1_score = len(output_lines_len)
     return part_1_score
 
-def get_decoder(potential:dict):
+
+def deduce_signals(full_line: list):
+    potential = {}
+
+    # First, start with the signals we can immediately attribute to a number (unique length)
+    for signal in full_line:
+        signal = set(signal)
+
+        if len(signal) == 2:
+            potential[1] = signal
+        elif len(signal) == 3:
+            potential[7] = signal
+        elif len(signal) == 4:
+            potential[4] = signal
+        elif len(signal) == 7:
+            potential[8] = signal
+
+    # Then move on to signals with more involved logic
+    for signal in full_line:
+        signal = set(signal)
+        if len(signal) == 5:
+            if potential[4] and len(potential[4].difference(signal)) == 2:
+                potential[2] = signal
+            elif potential[1] and len(potential[1].difference(signal)) == 1:
+                potential[5] = signal
+            elif potential[1] and len(potential[1].difference(signal)) == 0:
+                potential[3] = signal
+            else:
+                raise Exception("Missing logic to detect len 5 signal code.")
+        elif len(signal) == 6:
+            if potential[1] and len(potential[1].difference(signal)) == 1:
+                potential[6] = signal
+            elif potential[4] and len(potential[4].difference(signal)) == 1:
+                potential[0] = signal
+            elif potential[4] and len(potential[4].difference(signal)) == 0:
+                potential[9] = signal
+            else:
+                raise Exception("Missing logic to detect len 6 signal code.")
+
+    return potential
+
+
+def get_decoder(potential: dict):
     """
     Returns a dictionary that maps signal patterns to digits.
     Needs sorting so we can match with output.
@@ -23,59 +73,26 @@ def get_decoder(potential:dict):
         if v:
             v_lst = list(v)
             v_lst.sort()
-            decoder[''.join(v_lst)] = k
+            decoder["".join(v_lst)] = k
     return decoder
 
+
+def get_output_code(line: str):
+    full_line = line.replace(" | ", " ").split(" ")
+    potential = deduce_signals(full_line)  # get int -> set(str)
+    decoder = get_decoder(potential)  # get str -> int
+
+    output_list = line.split(" | ")[1].split(" ")
+    output_list = [
+        "".join(sorted(s)) for s in output_list
+    ]  # sort it so can be used as decoder key
+    output_code = [str(decoder[segment]) for segment in output_list]
+    return int("".join(output_code))
+
+
 def part_2(_input):
-    part_2_score = 0
-
-    for line in _input:
-        full_line = line.replace(' | ', ' ')
-        potential = {}
-
-        # First, start with the signals we can immediately attribute to a number (unique length)
-        for signal in full_line.split(' '):
-            signal = set(signal)
-
-            if len(signal) == 2:
-                potential[1] = signal
-            elif len(signal) == 3:
-                potential[7] = signal
-            elif len(signal) == 4:
-                potential[4] = signal
-            elif len(signal) == 7:
-                potential[8] = signal
-
-        # Then move on to signals with more involved logic
-        for signal in full_line.split(' '):
-            signal = set(signal)
-            if len(signal) == 5:
-                if potential[4] and len(potential[4].difference(signal)) == 2:
-                    potential[2] = signal
-                elif potential[1] and len(potential[1].difference(signal)) == 1:
-                    potential[5] = signal
-                elif potential[1] and len(potential[1].difference(signal)) == 0:
-                    potential[3] = signal
-                else:
-                    raise Exception("Missing logic to detect len 5 signal code.")
-            elif len(signal) == 6:
-                if potential[1] and len(potential[1].difference(signal)) == 1:
-                    potential[6] = signal
-                elif potential[4] and len(potential[4].difference(signal)) == 1:
-                    potential[0] = signal
-                elif potential[4] and len(potential[4].difference(signal)) == 0:
-                    potential[9] = signal
-                else:
-                    raise Exception("Missing logic to detect len 6 signal code.")
-
-        # Decode the characters in the output, add to score
-        decoder = get_decoder(potential)
-        output_str = line.split(' | ')[1]
-        output_list = [''.join(sorted(s)) for s in output_str.split(' ')]
-        output_code = [str(decoder[segment]) for segment in output_list]
-        part_2_score += int(''.join(output_code))
-
-    return part_2_score
+    part_2_score = [get_output_code(line) for line in _input]
+    return sum(part_2_score)
 
 
 @timer
--- 2021/test_AOC8.py
+++ 2021/test_AOC8.py
@@ -1,4 +1,5 @@
-from AOC8 import main
+from AOC8 import main, get_output_code
+from _utils import read_input
 
 
 def test_main():
@@ -9,3 +10,11 @@ def test_main():
     part_1, part_2 = main("aoc8.txt")
     assert part_1 == 488
     assert part_2 == 1040429
+
+
+def test_get_output_code():
+    test_input = read_input("test_aoc8.txt")
+    expected_list = [8394, 9781, 1197, 9361, 4873, 8418, 4548, 1625, 8717, 4315]
+    zipped_list = zip(test_input, expected_list)
+    for line, expected_int in zipped_list:
+        assert get_output_code(line) == expected_int
