827ee8d32e0a5845bda81275ba65d0fa1a42b29d
I A <ia767@nyu.edu>
2021-12-09 07:36:43 -0500

    [2021-aoc9] adding simple test

---
+++ 2021/test_AOC9.py
@@ -0,0 +1,11 @@
+from AOC9 import main
+
+
+def test_main():
+    test_part_1, test_part_2 = main("test_aoc9.txt")
+    assert test_part_1 == 15
+    assert test_part_2 == 1134
+
+    part_1, part_2 = main("aoc9.txt")
+    assert part_1 == 532
+    assert part_2 == 1110780
