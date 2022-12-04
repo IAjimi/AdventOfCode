b2fbff7dfe4b95ec075e8f1a0679295e21e721fe
I A <ia767@nyu.edu>
2022-12-04 11:08:21 -0500

    [2022][day 3] refactor solution

--- 2022/AOC03.py
+++ 2022/AOC03.py
@@ -1,50 +1,39 @@
-from collections import defaultdict
-from typing import List, Tuple
-
-from _utils import read_input, timer
+from _utils import read_input, timer, Score
 
 
-def get_score(char):
+def get_score(char) -> int:
     if char.islower():
         return 1 + ord(char) - ord("a")
     else:
         return 27 + ord(char) - ord("A")
 
 
-def part_1(_input):
-
+def part_1(_input) -> int:
     total = 0
+
     for rucksack in _input:
         first_half_index = len(rucksack) // 2
-        first_half_set = {c for c in rucksack[:first_half_index]}
-
-        for char in rucksack[first_half_index:]:
-            if char in first_half_set:
-                total += get_score(char)
-                break  # only 1 dupe per rucksack, can appear multiple times in 2nd half
+        rucksack1 = set(rucksack[:first_half_index])
+        rucksack2 = set(rucksack[first_half_index:])
+        dupe = rucksack1 & rucksack2
+        total += sum({get_score(char) for char in dupe})
     return total
 
 
-def part_2(_input):
+def part_2(_input) -> int:
     total = 0
 
     for group_id in range(0, len(_input), 3):
-        group_sack = defaultdict(int)
-
-        for rucksack in _input[group_id : group_id + 3]:
-            unique_items = {c for c in rucksack}
-            for char in unique_items:
-                group_sack[char] += 1
-
-        dupe = {
-            char for char, n in group_sack.items() if n == 3
-        }  # dupe is in all 3 rucksacks
+        rucksack1 = set(_input[group_id])
+        rucksack2 = set(_input[group_id+1])
+        rucksack3 = set(_input[group_id+2])
+        dupe = rucksack1 & rucksack2 & rucksack3
         total += sum({get_score(c) for c in dupe})
     return total
 
 
 @timer
-def main(filename: str) -> Tuple[int, int]:
+def main(filename: str) -> Score:
     _input = read_input(filename)
     part_1_score = part_1(_input)
     part_2_score = part_2(_input)
--- 2022/README.md
+++ 2022/README.md
@@ -5,4 +5,5 @@ This repository contains Python solutions to the 2022 edition of [Advent of Code
 | --- | -------| -----| 
 | [1](https://adventofcode.com/2022/day/1) | **Calorie Counting** | [:star::star:](https://github.com/IAjimi/AdventOfCode/blob/master/2022/AOC01.py) | 
 | [2](https://adventofcode.com/2022/day/2) | **Rock Paper Scissors** | [:star::star:](https://github.com/IAjimi/AdventOfCode/blob/master/2022/AOC02.py) | 
-| [3](https://adventofcode.com/2022/day/3) | **Rucksack Reorganization** | [:star::star:](https://github.com/IAjimi/AdventOfCode/blob/master/2022/AOC03.py) | 
+| [3](https://adventofcode.com/2022/day/3) | **Rucksack Reorganization** | [:star::star:](https://github.com/IAjimi/AdventOfCode/blob/master/2022/AOC03.py) |
+| [4](https://adventofcode.com/2022/day/4) | **Camp Cleanup** | [:star::star:](https://github.com/IAjimi/AdventOfCode/blob/master/2022/AOC04.py) | 
