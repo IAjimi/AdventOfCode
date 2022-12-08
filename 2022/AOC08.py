936fe7b456aec42883e42030b78e73d21c80f3e3
I A <ia767@nyu.edu>
2022-12-08 09:46:17 -0500

    [2022][day 8] refactoring solution

--- 2022/AOC08.py
+++ 2022/AOC08.py
@@ -12,7 +12,7 @@ def safe_max(l: List[int]) -> int:
     return max(l) if l else 0
 
 
-def find_visible_tree(_input: List[List[int]]) -> Set[Point]:
+def find_visible_trees(_input: List[List[int]]) -> Set[Point]:
     visible = set()
     max_x, max_y = len(_input[0]) - 1, len(_input) - 1
 
@@ -22,7 +22,7 @@ def find_visible_tree(_input: List[List[int]]) -> Set[Point]:
                 visible.add((x, y))
             else:
                 left_max = safe_max(_input[x][:y])
-                right_max = safe_max(_input[x][y + 1 :])
+                right_max = safe_max(_input[x][y + 1:])
                 top_max = safe_max([_input[i][y] for i in range(x)])
                 bottom_max = safe_max([_input[i][y] for i in range(x + 1, max_x + 1)])
                 if (
@@ -44,22 +44,21 @@ def compute_scenic_score(start: Point, _input: List[List[int]]) -> int:
         (0, (0, -1), start),
     ]
     height = _input[start[0]][start[1]]
-    sol = []
+    score = 1
     max_x, max_y = len(_input[0]) - 1, len(_input) - 1
 
     while queue:
         steps, dir, pos = queue.pop()
-        dx, dy = dir
-        x, y = pos
+        x, y = pos[0] + dir[0], pos[1] + dir[1]
 
-        if not (0 <= x + dx <= max_x) or not (0 <= y + dy <= max_y):
-            sol.append(steps)
-        elif _input[x + dx][y + dy] >= height:
-            sol.append(steps + 1)
+        if not (0 <= x <= max_x) or not (0 <= y <= max_y):
+            score *= steps
+        elif _input[x][y] >= height:
+            score *= steps + 1
         else:
-            queue.append((steps + 1, dir, (x + dx, y + dy)))
+            queue.append((steps + 1, dir, (x, y)))
 
-    return product(sol)
+    return score
 
 
 def find_best_view(_input: List[List[int]], visible_trees: Set[Point]) -> int:
@@ -73,7 +72,7 @@ def find_best_view(_input: List[List[int]], visible_trees: Set[Point]) -> int:
 @timer
 def main(filename: str) -> Solution:
     _input = process_input(filename)
-    visible_trees = find_visible_tree(_input)
+    visible_trees = find_visible_trees(_input)
     part_1_solution = len(visible_trees)
     part_2_solution = find_best_view(_input, visible_trees)
     return part_1_solution, part_2_solution
--- README.md
+++ README.md
@@ -7,7 +7,7 @@ This repository contains Python solutions to [Advent of Code puzzles](https://ad
 | [2016](https://adventofcode.com/2016)   | 1-7, 9-10, 12, 14, 16, 19-20 | [27 :star:](https://github.com/IAjimi/AdventOfCode2020/tree/master/2016) | 
 | [2017](https://adventofcode.com/2017)   | 1-13, 15-25 | [47 :star:](https://github.com/IAjimi/AdventOfCode2020/tree/master/2017) | 
 | [2018](https://adventofcode.com/2018) | 1-3, 5, 7-9, 11-12, 14 | [23 :star:](https://github.com/IAjimi/AdventOfCode2020/tree/master/2018) |
-| [2019](https://adventofcode.com/2019) | 2, 5, 7, 9, 12-15, 17-21, 23, 25 | [46 :star:](https://github.com/IAjimi/AdventOfCode2020/tree/master/2018) |
+| [2019](https://adventofcode.com/2019) | 2, 5, 7, 9, 12-15, 17-21, 23, 25 | [46 :star:](https://github.com/IAjimi/AdventOfCode2020/tree/master/2019) |
 | [2020](https://adventofcode.com/2020) | 1-25 | [46 :star:](https://github.com/IAjimi/AdventOfCode2020/tree/master/2020) |
 | [2021](https://adventofcode.com/2021) | 1-25 | [50 :star:](https://github.com/IAjimi/AdventOfCode2020/tree/master/2021) |
-| [2022](https://adventofcode.com/2022) | 1-3 | [6 :star:](https://github.com/IAjimi/AdventOfCode2020/tree/master/2022) |
+| [2022](https://adventofcode.com/2022) | 1-8 | [16 :star:](https://github.com/IAjimi/AdventOfCode2020/tree/master/2022) |
