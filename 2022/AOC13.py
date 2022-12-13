54bff64059e89009565d2dbada279aa7ab03ae4d
I A <ia767@nyu.edu>
2022-12-13 09:00:36 -0500

    [2022][day 13] refactoring solution

--- 2022/AOC13.py
+++ 2022/AOC13.py
@@ -1,84 +1,54 @@
-from typing import List, Tuple, Set
+from typing import List
 
+from functools import cmp_to_key
 from _utils import read_input, timer, Solution, product
 
 
-def process_input(filename: str):
+def process_input(filename: str) -> List[list]:
     _input = read_input(filename)
     _input = [eval(l) for l in _input if l != ""]
     return _input
 
 
-def compare_pairs(pair1, pair2) -> bool:
+def compare_pairs(pair1, pair2) -> int:
     if type(pair1) == int and type(pair2) == int:
         if pair1 < pair2:
-            return True
+            return -1
         elif pair1 > pair2:
-            return False
+            return 1
         else:
-            return None
-
-    i = 0
-    while i < len(pair1) and i < len(pair2):
-        p1, p2 = pair1[i], pair2[i]
-        if type(p1) == int and type(p2) == int:
-            if p1 < p2:
-                return True
-            elif p1 > p2:
-                return False
-            else:
-                comp = None
-        elif type(p1) == list and type(p2) == int:
-            comp = compare_pairs(p1, [p2])
-        elif type(p1) == int and type(p2) == list:
-            comp = compare_pairs([p1], p2)
-        else:
+            return 0
+    elif type(pair1) == list and type(pair2) == int:
+        return compare_pairs(pair1, [pair2])
+    elif type(pair1) == int and type(pair2) == list:
+        return compare_pairs([pair1], pair2)
+    else:
+        i = 0
+        while i < len(pair1) and i < len(pair2):
+            p1, p2 = pair1[i], pair2[i]
             comp = compare_pairs(p1, p2)
-
-        if comp is not None:
-            return comp
-        else:
-            i += 1
+            if comp != 0:
+                return comp
+            else:
+                i += 1
 
     if len(pair1) < len(pair2):
-        return True
+        return -1
     elif len(pair1) > len(pair2):
-        return False
+        return 1
     else:
-        return None
+        return 0
 
 
 def part1(_input) -> int:
     _input = [compare_pairs(_input[i], _input[i + 1]) for i in range(0, len(_input), 2)]
-    return sum([1 + i if pair else 0 for i, pair in enumerate(_input)])
-
-
-class InsertionSort:
-    def insert(self, A: List[int], i: int) -> List[int]:
-        j = i - 1
-
-        while j >= 0 and compare_pairs(A[i], A[j]):
-            A = self.swap(A, i, j)
-            i -= 1
-            j -= 1
-
-        return A
-
-    def swap(self, A: List[int], i: int, j: int) -> List[int]:
-        A[i], A[j] = A[j], A[i]
-        return A
-
-    def sort(self, A: List[int]) -> List[int]:
-        for i in range(len(A)):
-            A = self.insert(A, i)
-
-        return A
+    return sum([1 + i if pair == -1 else 0 for i, pair in enumerate(_input)])
 
 
 def part2(_input) -> int:
     _input.append([[2]])
     _input.append([[6]])
-    _input = InsertionSort().sort(_input)
+    _input.sort(key=cmp_to_key(compare_pairs))
     return product([i + 1 for i, v in enumerate(_input) if v in ([[2]], [[6]])])
 
 
